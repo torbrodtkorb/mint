067495520430b7b098ac363a4dbe12b758821057
torbrodtkorb <tor.brodtkorb98@gmail.com>
2020-12-29 10:05:05 -1100

    Started on the entry code

--- bootloader/entry/entry.s
+++ bootloader/entry/entry.s
@@ -1,1 +1,38 @@
+@ Starup routine for the bootloader
 
+@use both arm and thumb
+.syntax unified
+.cpu cortex-a5
+.arm
+
+.section .arm_vetors, "ax"
+arm_vectors:
+    ldr pc, =reset
+    ldr pc, =undef
+    ldr pc, =supervisor
+    ldr pc, =prefetch_abort
+    ldr pc, =data_abort
+    .word 0
+    ldr pc, =irq
+    ldr pc, =fiq
+
+reset:
+    b .
+
+undef:
+    b .
+
+supervisor:
+    b .
+
+prefetch_abort:
+    b .
+
+data_abort:
+    b .
+
+irq:
+    b .
+
+fiq:
+    b .
